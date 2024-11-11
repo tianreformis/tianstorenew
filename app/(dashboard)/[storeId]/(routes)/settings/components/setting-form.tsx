a31e63fa71661cce6b77e9307a33d47f770e9bfa
Kristian Reformis <60687151+tianreformis@users.noreply.github.com>
2024-11-11 16:13:22 +0800

    adding data

--- app/(dashboard)/[storeId]/(routes)/settings/components/setting-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/setting-form.tsx
@@ -1,8 +1,67 @@
-const SettingsForm = () => {
+'use client'
+
+import * as z from 'zod'
+
+import { Button } from "@/components/ui/button";
+import { Heading } from "@/components/ui/heading";
+import { Separator } from "@/components/ui/separator";
+import { Store } from "@prisma/client";
+import { Trash } from "lucide-react";
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useState } from 'react';
+import { Form } from '@/components/ui/form';
+
+interface settingsFormProps {
+    initialData: Store;
+}
+
+const formSchema = z.object({
+    name: z.string().min(1),
+})
+
+type SettingsFormValues = z.infer<typeof formSchema>;
+
+const SettingsForm: React.FC<settingsFormProps> = ({
+    initialData,
+}) => {
+    const [open, setOpen] = useState(false);
+    const [loading, setLoading] = useState(false);
+
+    const form = useForm<SettingsFormValues>({
+        resolver: zodResolver(formSchema),
+        defaultValues: initialData,
+    });
+
+    const onSubmit = async (data: SettingsFormValues) => {
+        console.log(data);
+    };
+
     return (
-        <div className="settings-form">
-            Komponen
-        </div>
+        <>
+
+            <div className="flex items-center justify-between">
+
+                <Heading
+                    title="Settings"
+                    description="Manage store preferences"
+                />
+                <Button
+                    variant="destructive"
+                    size='sm'
+                    onClick={() => { }}
+                >
+                    <Trash className="h-4 w-4" />
+                </Button>
+            </div>
+            <Separator />
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+                    
+
+                </form>
+            </Form>
+        </>
     )
 }
 
--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -30,8 +30,11 @@ const SettingsPage: React.FC<SettingPageProps> = async ({
     }
 
     return (
-        <div className="">
-            <SettingsForm />
+        <div className="flex-col">
+
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                <SettingsForm initialData={store} />
+            </div>
         </div>
 
     )
